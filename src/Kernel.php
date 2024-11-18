cfdd65de90fa13d7c8279bb7c093ed1a0236ee7c
Nicolas Joubert <njoubert@clever-age.com>
2024-11-18 16:01:57 +0100

    #1 Add notifications, quality & test github workflows. Apply rules.

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CleverAge/ProcessBundleDemo package.
+ *
+ * Copyright (c) Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
