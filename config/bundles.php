b75f683ba51d2622541765e1ec2371bc7d81bf87
Nicolas Joubert <njoubert@clever-age.com>
2024-11-27 11:05:18 +0100

    #10 composer require cleverage/soap-process-bundle:'dev-prepare-release'

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     CleverAge\DoctrineProcessBundle\CleverAgeDoctrineProcessBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    CleverAge\SoapProcessBundle\CleverAgeSoapProcessBundle::class => ['all' => true],
 ];
