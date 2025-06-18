7ede02359f2b9c82cf75df5f250f409bd6715e1a
Nicolas Joubert <njoubert@clever-age.com>
2025-06-18 15:28:30 +0200

    Update project with latest recipes

---
+++ src/Schedule.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the CleverAge/ProcessBundleDemo package.
+ *
+ * Copyright (c) Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
+#[AsSchedule]
+class Schedule implements ScheduleProviderInterface
+{
+    public function __construct(
+        private readonly CacheInterface $cache,
+    ) {
+    }
+
+    public function getSchedule(): SymfonySchedule
+    {
+        return (new SymfonySchedule())
+            ->stateful($this->cache) // ensure missed tasks are executed
+            ->processOnlyLastMissedRun(true) // ensure only last missed task is run
+
+            // add your own tasks here
+            // see https://symfony.com/doc/current/scheduler.html#attaching-recurring-messages-to-a-schedule
+        ;
+    }
+}
